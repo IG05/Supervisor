c2ee6d6e9fb498236ff897e84dba700274f56f09
IG05 <ishaan2004goyal@gmail.com>
2025-06-03 13:02:16 +0530

    second commit

--- app/main.py
+++ app/main.py
@@ -2,12 +2,19 @@ from flask import request
 from __init__ import make_flask_app
 from tasks import add
 import logging
+from prometheus_flask_exporter import PrometheusMetrics
+from prometheus_client import Counter
 
 
 app = make_flask_app()
+metrics = PrometheusMetrics(app)
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+celery_tasks_submitted = Counter('celery_tasks_submitted_total', 'Total Celery tasks submitted')
+
 
 @app.route('/')
 def hello():
@@ -19,6 +26,7 @@ def call_add():
     x = int(request.args.get('x', 1))
     y = int(request.args.get('y', 2))
     result = add.delay(x, y)
+    celery_tasks_submitted.inc()  # increment counter when task is submitted
     logger.info(f"Submitted task: add({x}, {y})")
     return f"Task submitted: add({x}, {y})"
 
